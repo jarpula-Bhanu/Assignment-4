314653f242934aec5002268110480217634c2740
jarpula-Bhanu <102509069+jarpula-Bhanu@users.noreply.github.com>
2022-04-19 12:59:49 +0530

    Update pro.py

--- codes/pro.py
+++ codes/pro.py
@@ -7,9 +7,9 @@ ctr_0 = np.count_nonzero(x==0)
 ctr_2 = np.count_nonzero(x==2)
 ctr_1 = N - ctr_0 - ctr_2
 
-pr_0 = 0.21
-pr_1 = 0.55
-pr_2 = 0.24
+pr_0 = 105/500
+pr_1 = 275/500
+pr_2 = 120/500
 
 print("Theoritical probabilities are",pr_0,pr_1,pr_2)
 print("Expermental probabilities are",ctr_0/N,ctr_1/N,ctr_2/N)
