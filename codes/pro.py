d931b908b47896b3603d7bba0119d03462eee933
jarpula-Bhanu <102509069+jarpula-Bhanu@users.noreply.github.com>
2022-04-19 21:57:34 +0530

    Add files via upload

--- codes/pro.py
+++ codes/pro.py
@@ -1,15 +1,21 @@
 import numpy as np
 from numpy import random as rn
 
-N = 500
+N = 500 #sample space
 x = rn.randint(0,4,size=N)
 ctr_0 = np.count_nonzero(x==0)
 ctr_2 = np.count_nonzero(x==2)
 ctr_1 = N - ctr_0 - ctr_2
 
-pr_0 = 105/500
-pr_1 = 275/500
-pr_2 = 120/500
+pr_0 = 0.25     
+pr_1 = 0.5
+pr_2 = 0.25
+
+################################
+# pr(zero head) = T T          #
+# pr(one head)  = T H and H T  #
+# pr(two head)  = H H          #
+################################
 
 print("Theoritical probabilities are",pr_0,pr_1,pr_2)
 print("Expermental probabilities are",ctr_0/N,ctr_1/N,ctr_2/N)
