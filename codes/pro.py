f2b8102601a940422d8070a5dc64b8cf1f66a282
jarpula-Bhanu <102509069+jarpula-Bhanu@users.noreply.github.com>
2022-04-21 19:30:30 +0530

    Add files via upload

--- codes/pro.py
+++ codes/pro.py
@@ -2,14 +2,14 @@ import numpy as np
 from numpy import random as rn
 
 N = 500 #sample space
-x = rn.randint(0,4,size=N)
-ctr_0 = np.count_nonzero(x==0)
+x = rn.randint(0,3,size=N) #considering coins are fair 
+ctr_1 = np.count_nonzero(x==1)
 ctr_2 = np.count_nonzero(x==2)
-ctr_1 = N - ctr_0 - ctr_2
+ctr_0 = N - ctr_1 - ctr_2
 
-pr_0 = 0.25     #probability of getting zero head
-pr_1 = 0.5      #probability of getting one head
-pr_2 = 0.25     #probability of getting two heads
+pr_0 = 0.25     
+pr_1 = 0.5
+pr_2 = 0.25
 
 ################################
 # pr(zero head) = T T          #
